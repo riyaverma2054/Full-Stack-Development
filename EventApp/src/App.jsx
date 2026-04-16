3cfab44b05d3b7f94aa188ded1e4522f17b36be0
Riya Verma <riya.work2054@gmail.com>
2026-04-16 23:51:44 +0530

    Fixed EventApp folder (removed nested git)

---
+++ EventApp/src/App.jsx
@@ -0,0 +1,33 @@
+
+//  //import EventToDo from "./EventToDo";
+// //  import Counter from "./Counter";
+//  import Search from "./Search";
+//   export default function App()
+//   {
+
+
+//   return (
+//     <>
+//     {/* <EventToDo/> */}
+//      {/* <Counter/> */}
+//      <Search/>
+   
+//     </>
+//   );
+// }
+
+import { useEffect } from "react";
+import './App.css'
+
+function App(){
+   useEffect(()=>{console.log("Welcome"),[]})
+
+   //useEffect(()=>{console.log("Count update"),[count]})
+
+return (
+  <>
+  <h1> My App </h1>
+
+  </>
+)
+}
